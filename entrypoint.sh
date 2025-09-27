5f2a0fad248d9ae9db2c122426bd0ede33667068
Rahul Meshram <bsmsus@gmail.com>
2025-09-27 18:57:20 +0530

    Task B: conf changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,15 @@ echo "✅ Using PORT=$PORT"
 
 # Ensure Laravel cache dirs exist
 mkdir -p storage/framework/{cache,sessions,views} bootstrap/cache
+
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache storage/framework
+
+# Clear stale caches
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
 
 # Replace $PORT in nginx.conf
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
